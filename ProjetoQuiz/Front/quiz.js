0c16c9545ada03e947ab9229eb1cd6d846140110
Gustavo Andrade <gustavo20martinss@gmail.com>
2024-10-09 23:12:23 -0300

    updates de melhoria de responsividade, aprendizado, e melhores entendimentos sobre frontend

--- ProjetoQuiz/Front/quiz.js
+++ ProjetoQuiz/Front/quiz.js
@@ -1,21 +1,7 @@
 const buttons = document.querySelectorAll('button');
 
 buttons.forEach(button => {
-  const originalColor = button.style.backgroundColor;
-  const borderColor = button.style.borderColor;
-
-  button.addEventListener('mouseover', () => {
-    button.style.backgroundColor = borderColor;
-  });
-
-  button.addEventListener('mouseout', () => {
-    
-    if (!button.classList.contains('clicked')) {
-      button.style.backgroundColor = originalColor;
-    }
-  });
-
   button.addEventListener('click', () => {
-    this.classList.add('clicked');
+    button.style.backgroundColor = button.style.borderColor;
   });
 });
